0c34bb3a846fe6c956cb8b083a46472386132b07
Engineral <lbraff03@gmail.com>
2023-07-03 18:47:33 -0400

    Moved `bot.run(token)` from before the @event to after the @event

--- commands.py
+++ commands.py
@@ -3,22 +3,23 @@ from datetime import *
 
 token = 'nul'   # Replace nul with corresponding token
 
-# intents = dpy.Intents.default()
-# intents.message_content = True
+intents = dpy.Intents.default()
+intents.message_content = True
 
-# bot = dpy.Client(intents=intents)   # 'bot' substitutes for 'client'
-# bot.run(token)
+bot = dpy.Client(intents=intents)   # 'bot' substitutes for 'client'
 
-# @bot.event
-# async def on_ready():
-#     print(f'We have logged in as {bot.user}')
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
     
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+bot.run(token)
 
 print(date.today())
