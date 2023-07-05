5167780ba64960bedebcb0e47506addbdbcd810d
Engineral <lbraff03@gmail.com>
2023-07-04 21:33:32 -0400

    Imported dev branch to master branch

--- commands.py
+++ commands.py
@@ -9,19 +9,45 @@ intents.message_content = True
 
 bot = dpy.Client(intents=intents)   # 'bot' substitutes for 'client'
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+try:
+    @bot.event
+    async def on_ready():
+        ping = str(datetime.now())
+        print(ping[0:19], f'We have logged in as {bot.user}')
 
+        channel = bot.get_channel(1092234344535445534)
+        await channel.send('**おはよございます**\nFujiwara-Chan, signing on!')
+
+    """
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+async def on_connect():
+    channel = bot.get_channel(1092234344535445534)
+    await channel.send('**おはよございます**\nFujiwara-Chan, signing on!')
+    print(datetime.now(), f'{bot.user} is now appearing online')
+"""
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
     
-    if message.content.startswith(prefix + 'hello'):
-        await message.channel.send('Hello!')
+        if message.content.startswith(f'{prefix}call'):
+            print('  Cmd Called:   \'+call\'\nCmd Response:   \'response\'')
 
+            await message.channel.send('response')
+except KeyboardInterrupt:
+    ping = str(datetime.now())
+    print(ping[0:19], f'{bot.user} has disconnected')
+
+    # channel = bot.get_channel(1092234344535445534)
+    # channel.send('**おやすみなさい**\nFujiwara-Chan, signing off!')
+
+# @bot.event
+# async def on_disconnect():
+#     ping = str(datetime.now())
+#     print(ping[0:19], f'{bot.user} has disconnected')
+
+#     channel = bot.get_channel(1092234344535445534)
+#     await channel.send('**おやすみなさい**\nFujiwara-Chan, signing off!')
 
 bot.run(token)
-
-print(date.today())
