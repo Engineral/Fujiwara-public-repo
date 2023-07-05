584794ab20dbab5feeef999c1ec6b96953b2d05a
Engineral <lbraff03@gmail.com>
2023-07-05 00:32:06 -0400

    Tested call & response limitations

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 import discord as dpy
+import random
 from datetime import *
 
 token = 'nul'   # Replace nul with corresponding token
@@ -9,16 +10,16 @@ intents.message_content = True
 
 bot = dpy.Client(intents=intents)   # 'bot' substitutes for 'client'
 
-try:
-    @bot.event
-    async def on_ready():
-        ping = str(datetime.now())
-        print(ping[0:19], f'We have logged in as {bot.user}')
 
-        channel = bot.get_channel(1092234344535445534)
-        await channel.send('**おはよございます**\nFujiwara-Chan, signing on!')
+@bot.event
+async def on_ready():
+    ping = str(datetime.now())
+    print(ping[0:19], f'We have logged in as {bot.user}')
 
-    """
+    channel = bot.get_channel(1092234344535445534)
+    await channel.send('**おはよございます**\nFujiwara-Chan, signing on!')
+
+"""
 @bot.event
 async def on_connect():
     channel = bot.get_channel(1092234344535445534)
@@ -26,28 +27,47 @@ async def on_connect():
     print(datetime.now(), f'{bot.user} is now appearing online')
 """
 
-    @bot.event
-    async def on_message(message):
-        if message.author == bot.user:
-            return
+# Commands (not to be confused with @bot.commands)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
     
-        if message.content.startswith(f'{prefix}call'):
-            print('  Cmd Called:   \'+call\'\nCmd Response:   \'response\'')
+    """
+    Prefix: +
+    Commands:
+        help - lists all commands
+        call - response
+        coinflip - flips a coin
+    """
+    
+    if message.content.startswith(f'{prefix}help'):
+        await message.channel.send(embed=dpy.Embed(title='Commands', description='List of Fujiwara Commands\n\nhelp - lists all commands\ncall - response\ncoinflip - flips a coin', color=0x00ff00))
 
-            await message.channel.send('response')
-except KeyboardInterrupt:
+    if message.content.startswith(f'{prefix}call'):
+        print('  Cmd Called:   \'+call\'\nCmd Response:   \'response\'')
+
+        await message.channel.send('response')
+    
+    if message.content.startswith(f'{prefix}coinflip'):
+        rng = random.randint(0,1)
+
+        coin = 'nul'
+        if rng == 0:
+            coin = 'Heads'
+        elif rng == 1:
+            coin = 'Tails'
+        else:
+            coin = 'nul'
+
+        await message.channel.send(coin)
+
+@bot.event
+async def on_disconnect():
     ping = str(datetime.now())
     print(ping[0:19], f'{bot.user} has disconnected')
 
-    # channel = bot.get_channel(1092234344535445534)
-    # channel.send('**おやすみなさい**\nFujiwara-Chan, signing off!')
-
-# @bot.event
-# async def on_disconnect():
-#     ping = str(datetime.now())
-#     print(ping[0:19], f'{bot.user} has disconnected')
-
-#     channel = bot.get_channel(1092234344535445534)
-#     await channel.send('**おやすみなさい**\nFujiwara-Chan, signing off!')
+    channel = bot.get_channel(1092234344535445534)
+    await channel.send('**おやすみなさい**\nFujiwara-Chan, signing off!')
 
 bot.run(token)
